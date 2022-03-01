ea1038a1d801abd837cfe16903977b59bfe80c3b
Sakib Md Rezowan <89691048+RezowanSakib@users.noreply.github.com>
2022-03-01 12:39:55 +0600

    cards coplete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
-const getName=()=>{
-   let inputPhone= document.getElementById('phone-name');
-   let inputPhoneText= document.getElementById('phone-name').value;
-  inputPhone.value='';
-  return inputPhoneText;
-}
-const loadPhone = () => {
-   const url=` https://openapi.programming-hero.com/api/phones?search=${getName()}`
+const getName = () => {
+  let inputPhone = document.getElementById("phone-name");
+  let inputPhoneText = document.getElementById("phone-name").value;
+  inputPhone.value = "";
+  const url = ` https://openapi.programming-hero.com/api/phones?search=${inputPhoneText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayPhone(data.data));
+};
+
+const displayPhone = (phones) => {
+ 
+  const searchResult=document.getElementById('result');
+  phones.forEach(phone=>{
+   console.log(phone);
+   const div=document.createElement('div');
+   div.classList.add('col');
+   div.innerHTML=`<div class="col">
+   <div class="card h-100">
+     <img src="${phone.image}" class="card-img-top" alt="..." />
+     <div class="card-body">
+       <h5 class="card-title">${phone.phone_name}</h5>
+       <p class="card-text">
+         ${phone.slug}.
+       </p>
+     </div>
+   </div>
+ </div>`;
+ searchResult.appendChild(div);
+  })
+ 
 };
